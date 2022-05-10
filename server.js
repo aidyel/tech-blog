8c48d482a21480651c13949af4129622164b24d4
aidyel <aidayeli@icloud.com>
2022-05-09 22:32:09 -0400

    added helpers

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 const session = require('express-session');
 const path = require('path');
+const helpers = require('./utils/helpers');
+
 
 
 const app = express();
@@ -24,6 +26,8 @@ const sess = {
 app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
