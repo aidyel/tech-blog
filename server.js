4a1f199309161b6ebdfb54431d17f5ca39cecc1f
aidyel <aidayeli@icloud.com>
2022-05-09 22:15:21 -0400

    single post

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const session = require('express-session');
+const path = require('path');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,6 +27,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
